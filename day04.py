aa6f0dbcdfbdbae822faf001f4e521f9b29f7012
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-04 12:43:05 +0000

    Solve day 4

--- day04.py
+++ day04.py
@@ -1,18 +1,36 @@
 import argparse
+import copy
+import itertools
 
 
 def parse(fh):
-    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
+    data = set((col, row) for row, line in enumerate(fh) for col, char in enumerate(line.strip()) if char == "@")
 
     return data
 
 
+def neighbors(pt):
+    x, y = pt
+    return ((x + dx, y + dy) for dx, dy in itertools.product((-1, 0, 1), repeat=2))
+
+
+def accessible(pt, data):
+    return sum(xy in data for xy in neighbors(pt)) <= 4
+
+
 def part1(data):
-    return 0
+    return sum(accessible(pt, data) for pt in data)
 
 
 def part2(data):
-    return 0
+    start = len(data)
+    new = set(pt for pt in data if not accessible(pt, data))
+
+    while new != data:
+        data = new
+        new = set(pt for pt in data if not accessible(pt, data))
+
+    return start - len(data)
 
 
 if __name__ == "__main__":
@@ -21,7 +39,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.filename) as fh:
-        data = parse(fh)
+        data = parse(fh.readlines())
 
     p1 = part1(data)
     p2 = part2(data)
