e2fb5a8704387ab3dd89554a17ce3719f0b1952f
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-06 08:05:27 +0000

    Solve part 1

--- day06.py
+++ day06.py
@@ -1,14 +1,32 @@
 import argparse
+import collections
+import functools
+import re
 
 
 def parse(fh):
-    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
+    lines = fh.readlines()
+
+    data = collections.defaultdict(list)
+    for line in lines[:-1]:
+        for col, n in enumerate(map(int, re.split(r" +", line.strip()))):
+            data[col].append(n)
+
+    for col, op in enumerate(re.split(r" +", lines[-1].strip())):
+        data[col].append(op)
 
     return data
 
 
+def op(column):
+    if column[-1] == "+":
+        return sum(column[:-1])
+    else:
+        return functools.reduce(lambda x, y: x * y, column[:-1], 1)
+
+
 def part1(data):
-    return 0
+    return sum(map(op, data.values()))
 
 
 def part2(data):
