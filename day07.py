ac03ef9ce291894bfd08c4aa1d0ff3b95658ecfe
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-07 16:15:13 +0000

    Solve part 1

--- day07.py
+++ day07.py
@@ -1,18 +1,78 @@
 import argparse
+import collections
 
 
 def parse(fh):
-    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
-
-    return data
+    return [line.strip() for line in fh if "S" in line or "^" in line]
 
 
 def part1(data):
-    return 0
+    def step(row, beams):
+        if not beams:
+            i = row.find("S")
+            assert i != -1
+            return set([i]), 0
+
+        nxt = set()
+        splits = 0
+        for i in beams:
+            if row[i] == "^":
+                nxt.add(i - 1)
+                nxt.add(i + 1)
+                splits += 1
+            else:
+                nxt.add(i)
+
+        return nxt, splits
+
+    splits = 0
+    beams = None
+    for row in data:
+        beams, s = step(row, beams)
+        splits += s
+
+    return splits
+
+
+def step(row, beams):
+    if not beams:
+        i = row.find("S")
+        assert i != -1
+        return {i: 1}
+
+    nxt = collections.defaultdict(int)
+    for i, val in beams.items():
+        if row[i] == "^":
+            nxt[i - 1] += val
+            nxt[i + 1] += val
+        else:
+            nxt[i] = val
+
+    return nxt
 
 
 def part2(data):
-    return 0
+    def step(row, beams):
+        if not beams:
+            i = row.find("S")
+            assert i != -1
+            return {i: 1}
+
+        nxt = collections.defaultdict(int)
+        for i, val in beams.items():
+            if row[i] == "^":
+                nxt[i - 1] += val
+                nxt[i + 1] += val
+            else:
+                nxt[i] = val
+
+        return nxt
+
+    beams = {}
+    for row in data:
+        beams = step(row, beams)
+
+    return sum(v for v in beams.values())
 
 
 if __name__ == "__main__":
