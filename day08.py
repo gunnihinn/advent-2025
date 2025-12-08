bf4d5e4233fba83d745720da1b5509a69a94b3ff
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-08 09:29:42 +0000

    Solve day 8

--- day08.py
+++ day08.py
@@ -1,18 +1,69 @@
 import argparse
+import collections
+import functools
+import heapq
+import itertools
 
 
 def parse(fh):
-    data = [tuple(map(int, line.split(","))) for line in fh.readlines()]
+    points = sorted([tuple(map(int, line.split(","))) for line in fh.readlines()])
 
-    return data
+    def d(pt1, pt2):
+        return sum((x1 - x2) ** 2 for x1, x2 in zip(pt1, pt2))
+
+    dist = {(p, q): d(p, q) for p, q in itertools.combinations(points, 2)}
+
+    return points, dist
 
 
-def part1(data):
-    return 0
+def join(colors, p, q):
+    cp = colors[p]
+    cq = colors[q]
+    color = min(cp, cq)
+    new = {}
+    for k, v in colors.items():
+        if v == cp:
+            new[k] = color
+        elif v == cq:
+            new[k] = color
+        else:
+            new[k] = v
+
+    return new
+
+
+def part1(data, test: bool):
+    iters = 10 if test else 1000
+    topk = 3
+
+    points, dist = data
+    colors = {pt: i for i, pt in enumerate(points)}
+
+    heap = []
+    for pq, d in dist.items():
+        heapq.heappush(heap, (d, pq))
+
+    for _, (p, q) in heapq.nsmallest(iters, heap):
+        colors = join(colors, p, q)
+
+    circuits = collections.Counter(colors.values())
+
+    return functools.reduce(lambda x, y: x * y[1], circuits.most_common(topk), 1)
 
 
 def part2(data):
-    return 0
+    points, dist = data
+    colors = {pt: i for i, pt in enumerate(points)}
+
+    heap = []
+    for pq, d in dist.items():
+        heapq.heappush(heap, (d, pq))
+
+    while heap:
+        _, (p, q) = heapq.heappop(heap)
+        colors = join(colors, p, q)
+        if len(set(colors.values())) == 1:
+            return p[0] * q[0]
 
 
 if __name__ == "__main__":
@@ -23,7 +74,7 @@ if __name__ == "__main__":
     with open(args.filename) as fh:
         data = parse(fh)
 
-    p1 = part1(data)
+    p1 = part1(data, "test" in args.filename)
     p2 = part2(data)
 
     print(f"part1: {p1}")
