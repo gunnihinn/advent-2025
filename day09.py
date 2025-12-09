b61225337de30aa8f189eb618fad9d0e8575bd7a
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-09 15:01:41 +0000

    Attempt part 2

--- day09.py
+++ day09.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
 import argparse
+import datetime
+import functools
 import itertools
+import multiprocessing
+
+is_inside = None
 
 
 def parse(fh):
-    data = [tuple(map(int, line.split(","))) for line in fh.readlines()]
+    data = tuple([tuple(map(int, line.split(","))) for line in fh.readlines()])
 
     return data
 
@@ -16,8 +23,70 @@ def part1(data):
     return max(area(p, q) for p, q in itertools.combinations(data, 2))
 
 
+def edges(data):
+    return zip(data, itertools.islice(itertools.cycle(data), 1, None))
+
+
+def interior(p, q):
+    px, py = p
+    qx, qy = q
+
+    return itertools.product(range(min(px, qx), max(px, qx) + 1), range(min(py, qy), max(py, qy) + 1))
+
+
+def make_is_inside(data):
+    @functools.cache
+    def is_inside(xy):
+        # Cast a ray to the right and count how many times it intersects the edges
+        x, y = xy
+        for p, q in edges(data):
+            px, py = p
+            qx, qy = q
+
+            mx, Mx = min(px, qx), max(px, qx)
+            my, My = min(py, qy), max(py, qy)
+
+            if mx <= x <= Mx and my <= y <= My:
+                return True
+
+        return sum(index(xy, p, q) for p, q in edges(data)) % 2
+
+    return is_inside
+
+
+def index(xy, p, q):
+    x, y = xy
+    px, py = p
+    qx, qy = q
+
+    mx, Mx = min(px, qx), max(px, qx)
+    my, My = min(py, qy), max(py, qy)
+
+    if px == qx:
+        return x <= px and my <= y < My
+    else:
+        return y == py and mx <= x <= Mx
+
+
+def contained_in(p, q, is_inside):
+    return all(is_inside(x) for x in interior(p, q))
+
+
+def calc(pq):
+    if all(is_inside(x) for x in interior(pq[0], pq[1])):
+        print(f"{datetime.datetime.now()}: checked {pq}")
+        return area(pq[0], pq[1])
+    else:
+        print(f"{datetime.datetime.now()}: checked {pq}")
+        return 0
+
+
 def part2(data):
-    return 0
+    global is_inside
+    is_inside = make_is_inside(data)
+
+    with multiprocessing.Pool(10) as pool:
+        return max(pool.map(calc, itertools.combinations(data, 2)))
 
 
 if __name__ == "__main__":
@@ -29,7 +98,7 @@ if __name__ == "__main__":
         data = parse(fh)
 
     p1 = part1(data)
-    p2 = part2(data)
-
     print(f"part1: {p1}")
+
+    p2 = part2(data)
     print(f"part2: {p2}")
