aaca78c8dd3c491064fd51af0e46c564de32d46a
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-10 11:04:51 +0000

    Solve part 2 way too slowly

--- day10.py
+++ day10.py
@@ -37,8 +37,52 @@ def part1(data):
     return sum(presses(indicators, wirings) for indicators, wirings, _ in data)
 
 
+def step(point, joltage, wiring_increments, steps, best):
+    if point == joltage:
+        return steps
+
+    if steps >= best:
+        return best
+
+    neighbors = []
+    for inc in wiring_increments:
+        pt = tuple(p + i for p, i in zip(point, inc))
+        if leq(pt, joltage):
+            neighbors.append(pt)
+    if not neighbors:
+        return best
+
+    neighbors.sort(key=lambda pt: sum(abs(p - j) for p, j in zip(pt, joltage)))
+
+    got = best
+    for pt in neighbors:
+        got = min(got, step(pt, joltage, wiring_increments, steps + 1, best))
+        if got < best:
+            best = got
+            print(f".... joltage={joltage}, best={best}")
+
+    return got
+
+
+def joltage_presses(joltage, wirings):
+    start = tuple([0] * len(joltage))
+    wirings = sorted(wirings, key=lambda w: len(w), reverse=True)
+    wiring_increments = [tuple(i in wire for i in range(len(joltage))) for wire in wirings]
+
+    return step(start, joltage, wiring_increments, 0, 1_000_000_000_000_000)
+
+
+def leq(xs, ys):
+    return all(x <= y for x, y in zip(xs, ys))
+
+
 def part2(data):
-    return 0
+    s = 0
+    for i, (_, wirings, joltage) in enumerate(data, start=1):
+        print(f".. wiring {i}/{len(data)}")
+        s += joltage_presses(joltage, wirings)
+
+    return s
 
 
 if __name__ == "__main__":
@@ -50,7 +94,7 @@ if __name__ == "__main__":
         data = parse(fh)
 
     p1 = part1(data)
-    p2 = part2(data)
-
     print(f"part1: {p1}")
+
+    p2 = part2(data)
     print(f"part2: {p2}")
