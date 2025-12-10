787238aaff26c03f143f15fa4e9c548cf2b6032c
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-10 07:06:57 +0000

    Start day 10

---
+++ day10.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def parse(fh):
+    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
+
+    return data
+
+
+def part1(data):
+    print(data)
+    return 0
+
+
+def part2(data):
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as fh:
+        data = parse(fh)
+
+    p1 = part1(data)
+    p2 = part2(data)
+
+    print(f"part1: {p1}")
+    print(f"part2: {p2}")
