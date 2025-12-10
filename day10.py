40ce09eea8afb36cfba41a0c589be801ef0b6ed3
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-10 08:25:12 +0000

    Solve part 1

--- day10.py
+++ day10.py
@@ -1,15 +1,40 @@
 import argparse
+import functools
+import itertools
 
 
 def parse(fh):
-    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
+    data = []
+    for line in fh:
+        parts = line.strip().split(" ")
+
+        indicators = tuple(c == "#" for c in parts[0][1:-1])
+        wirings = tuple(tuple(map(int, part[1:-1].split(","))) for part in parts[1:-1])
+        joltage = tuple(map(int, parts[-1][1:-1].split(",")))
+
+        data.append((indicators, wirings, joltage))
 
     return data
 
 
+def presses(indicators, wirings):
+    return next(
+        n
+        for n in range(1, len(wirings) + 1)
+        for combo in itertools.combinations(wirings, n)
+        if indicators == press(len(indicators), combo)
+    )
+
+
+def press(k, combo):
+    def p(lights, button):
+        return tuple(v ^ (i in button) for i, v in enumerate(lights))
+
+    return functools.reduce(p, combo, tuple(False for _ in range(k)))
+
+
 def part1(data):
-    print(data)
-    return 0
+    return sum(presses(indicators, wirings) for indicators, wirings, _ in data)
 
 
 def part2(data):
