62c8823f4cba48ac772a502bf2e339890055fd0b
Gunnar Þór Magnússon <gunnar@magnusson.io>
2025-12-11 07:20:49 +0000

    Solve day 11

--- day11.py
+++ day11.py
@@ -1,18 +1,36 @@
 import argparse
+import functools
+
+_data = None
 
 
 def parse(fh):
-    data = [tuple(map(int, line.strip())) for line in fh.readlines()]
+    data = {}
+    for line in fh:
+        i = line.find(":")
+        key = line[:i]
+        vals = line[i + 1 :].strip().split(" ")
+        data[key] = tuple(vals)
 
     return data
 
 
+@functools.cache
+def paths(start, end):
+    if start == end:
+        return 1
+
+    return sum(paths(p, end) for p in _data.get(start, []))
+
+
 def part1(data):
-    return 0
+    return paths("you", "out")
 
 
 def part2(data):
-    return 0
+    svc_dac_fft_out = paths("svr", "dac") * paths("dac", "fft") * paths("fft", "out")
+    svc_fft_dac_out = paths("svr", "fft") * paths("fft", "dac") * paths("dac", "out")
+    return svc_dac_fft_out + svc_fft_dac_out
 
 
 if __name__ == "__main__":
@@ -22,6 +40,9 @@ if __name__ == "__main__":
 
     with open(args.filename) as fh:
         data = parse(fh)
+
+    global _data
+    _data = data
 
     p1 = part1(data)
     p2 = part2(data)
